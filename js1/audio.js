256219710b3023b04f38a3e83c04c378910f3372
Ryan Moua <111842848+Ryanmoua001@users.noreply.github.com>
2023-05-14 23:36:16 -0700

    ho

---
+++ js1/audio.js
@@ -0,0 +1,30 @@
+const yuhbtn = document.getElementById('yuh-button')
+const fbibtn = document.getElementById('fbi-button')
+const demibtn = document.getElementById('demi-button')
+const krisbtn = document.getElementById('kris-button')
+const eyebtn = document.getElementById('eye-button')
+const milliebtn = document.getElementById('millie-button')
+
+    yuhbtn.addEventListener('click', () =>{
+        document.getElementById('yuh').play()
+    })
+
+    fbibtn.addEventListener('click', () =>{
+        document.getElementById('fbi').play()
+    })
+
+    demibtn.addEventListener('click', () =>{
+        document.getElementById('demi').play()
+    })
+
+    krisbtn.addEventListener('click', () =>{
+        document.getElementById('kris').play()
+    })
+
+    eyebtn.addEventListener('click', () =>{
+        document.getElementById('eye').play()
+    })
+
+    milliebtn.addEventListener('click', () =>{
+        document.getElementById('millie').play()
+    })
---
+++ js1/typing.js
@@ -0,0 +1,21 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text =  "Once upon a time there was a lovely princess"
+let idx = 1
+let speed = 300 / speedEl.value
+
+speedEl.addEventListener('input', (element) => {
+    speed = 300/element.target.value
+})
+
+writeText()
+
+function writeText(){
+    textEl.innerText = text.slice(0, idx)
+    idx++
+
+    if(idx > text.length){
+        idx = 1
+    }
+    setTimeout(writeText, speed)
+}
